1e595b665997677a3adc57da09dd3792188c6757
Moonkwanghyun <mgh3326@naver.com>
2018-10-25 13:16:23 +0900

    1025 update

--- first/소스.cpp
+++ first/소스.cpp
@@ -16,7 +16,7 @@ unsigned char** mem_alloc2_d(int n_height, int n_width, unsigned char nlnit_val)
 	for (int n = 0; n < n_height; n++)
 	{
 		rtn[n] = new unsigned char[n_width];
-		memset(rtn[n], nlnit_val, sizeof(unsigned char)*n_width);
+		memset(rtn[n], nlnit_val, sizeof(unsigned char) * n_width);
 	}
 	return rtn;
 }
@@ -65,7 +65,8 @@ unsigned char** padding(unsigned char** in, int n_height, int n_width, int n_fil
 
 	return pad;
 }
-void gausian_filter(unsigned char **in, unsigned char** Out, int nHeight, int nWidth, double sigma)
+
+void gausian_filter(unsigned char** in, unsigned char** Out, int nHeight, int nWidth, double sigma)
 {
 	unsigned char** pad = padding(in, nHeight, nWidth, 3);
 	int pad_size = 1;
@@ -117,13 +118,12 @@ void gausian_filter(unsigned char **in, unsigned char** Out, int nHeight, int nW
 			Out[h][w] = result;
 		}
 	}
-
-
 }
+
 void main()
 {
 	Mat Img_Input = imread("input.jpg", 1);
-	unsigned char** img_array_gray = mem_alloc2_d(Img_Input.rows, Img_Input.cols, 0);//가로 세로
+	unsigned char** img_array_gray = mem_alloc2_d(Img_Input.rows, Img_Input.cols, 0); //가로 세로
 	Mat Img_Gray(Size(Img_Input.cols, Img_Input.rows), 0);
 
 	for (int h = 0; h < Img_Input.rows; h++)
@@ -137,7 +137,7 @@ void main()
 	unsigned char** Out = mem_alloc2_d(Img_Input.rows, Img_Input.cols, 0);
 	unsigned char** Out2 = mem_alloc2_d(Img_Input.rows, Img_Input.cols, 0);
 
-	gausian_filter(img_array_gray, Out, Img_Input.rows, Img_Input.cols, 0.5);//가우시안이 어떤 효과를 가져오는지를 알아봐야겠다.
+	gausian_filter(img_array_gray, Out, Img_Input.rows, Img_Input.cols, 0.5); //가우시안이 어떤 효과를 가져오는지를 알아봐야겠다.
 	gausian_filter(img_array_gray, Out2, Img_Input.rows, Img_Input.cols, 5);
 	for (int h = 0; h < Img_Input.rows; h++)
 	{
@@ -157,30 +157,27 @@ void main()
 			if (Out[h][w] - Out2[h][w] > 255)
 			{
 				Img_Gray.at<uchar>(h, w) = 255;
-
 			}
-			else {
+			else
+			{
 				Img_Gray.at<uchar>(h, w) = (Out[h][w] - Out2[h][w]);
-
 			}
-
 		}
 	}
 	Mat Img_Binary(Size(Img_Input.cols, Img_Input.rows), 0);
 
-	for (int h = 0; h < Img_Input.rows; h++)//이진화
+	for (int h = 0; h < Img_Input.rows; h++) //이진화
 	{
 		for (int w = 0; w < Img_Input.cols; w++)
 		{
 			if (Img_Gray.at<uchar>(h, w) > 127)
 			{
 				Img_Binary.at<uchar>(h, w) = 0;
-
 			}
-			else {
+			else
+			{
 				Img_Binary.at<uchar>(h, w) = 255;
 			}
-
 		}
 	}
 	imshow("Ori", Img_Input);
@@ -188,142 +185,5 @@ void main()
 	imshow("Binary", Img_Binary);
 
 
-	//FILE *fp_in, *fp_out;
-	//fopen_s(&fp_in, "lena256.raw", "rb");
-	////fp_in = fopen("lena256.raw", "rb"); // 난 이렇게 했는데 여기서는 이렇게 했네
-	//unsigned char** Y = (unsigned char**)malloc(sizeof(unsigned char*) * HEIGHT);
-
-	//for (int h = 0; h < HEIGHT; h++)
-	//{
-	//	Y[h] = (unsigned char*)malloc(sizeof(unsigned char) * WIDTH);
-	//	fread(Y[h], sizeof(unsigned char), WIDTH, fp_in);
-	//}
-	//Mat Ori_Img = Mat(HEIGHT, WIDTH, CV_8UC1);
-
-	//for (int h = 0; h < HEIGHT; h++)
-
-	//{
-	//	for (int w = 0; w < WIDTH; w++)
-
-	//	{
-	//		Ori_Img.at<unsigned char>(h, w) = Y[h][w];
-	//	}
-	//}
-	//imshow("Ori", Ori_Img);
-
-
-	//unsigned char** temp;
-
-	//temp = (unsigned char**)malloc(sizeof(unsigned char*) * HEIGHT);
-
-	//for (int h = 0; h < HEIGHT; h++)
-	//{
-	//	temp[h] = (unsigned char*)malloc(sizeof(unsigned char) * WIDTH);
-	//	fread(temp[h], sizeof(unsigned char), WIDTH, fp_in);
-	//}
-	//for (int i = 0; i < HEIGHT; i++)
-	//{
-	//	for (int j = 0; j < WIDTH; j++)
-	//	{
-	//		temp[i][j] = 0;
-	//	}
-	//}
-	//Mat Img_squre = Mat(HEIGHT, WIDTH, CV_8UC1);
-
-	//for (int h = 0; h < HEIGHT; h++)
-
-	//{
-	//	for (int w = 0; w < WIDTH; w++)
-
-	//	{
-	//		Img_squre.at<unsigned char>(h, w) = temp[h][w];
-	//	}
-	//}
-	//imshow("Img_squre", Img_squre);
-
-	//for (int i = 0; i < HEIGHT; i++)
-	//{
-	//	for (int j = 0; j < WIDTH; j++)
-	//	{
-	//		temp[i][j] = 255;
-	//	}
-	//}
-	//for (int h = 0; h < HEIGHT; h++)
-
-	//{
-	//	for (int w = 0; w < WIDTH; w++)
-
-	//	{
-	//		Img_squre.at<unsigned char>(h, w) = temp[h][w];
-	//	}
-	//}
-	//imshow("Img_squre_white", Img_squre);
-
-	//for (int i = 0; i < HEIGHT; i++)
-	//{
-	//	for (int j = 0; j < WIDTH; j++)
-	//	{
-	//		/*if ((i<60 && i>HEIGHT > 60 )&& (j<WIDTH - 60 && j> 60))*/
-	//		if ((i > 50 && i < HEIGHT - 50 && j > 50 && j < WIDTH - 50))
-
-	//		{
-	//			temp[i][j] = 255;
-	//		}
-	//		else
-	//		{
-	//			temp[i][j] = 0;
-	//		}
-	//	}
-	//}
-	//for (int h = 0; h < HEIGHT; h++)
-
-	//{
-	//	for (int w = 0; w < WIDTH; w++)
-
-	//	{
-	//		Img_squre.at<unsigned char>(h, w) = temp[h][w];
-	//	}
-	//}
-	//imshow("Img_squre_white of squre", Img_squre);
-
-
-	//for (int i = 0; i < HEIGHT; i++)
-	//{
-	//	for (int j = 0; j < WIDTH; j++)
-	//	{
-	//		/*if ((i<60 && i>HEIGHT > 60 )&& (j<WIDTH - 60 && j> 60))*/
-	//		if (((double)HEIGHT / 2 - i) * ((double)HEIGHT / 2 - i) + ((double)WIDTH / 2 - j) * ((double)WIDTH / 2 - j)
-	//			<= 100 * 100)
-
-
-	//		{
-	//			temp[i][j] = 255;
-	//		}
-	//		else
-	//		{
-	//			temp[i][j] = 0;
-	//		}
-	//	}
-	//}
-	//for (int h = 0; h < HEIGHT; h++)
-
-	//{
-	//	for (int w = 0; w < WIDTH; w++)
-
-	//	{
-	//		Img_squre.at<unsigned char>(h, w) = temp[h][w];
-	//	}
-	//}
-	//imshow("Circle Image", Img_squre);
-	//double dScaleRate = 0;
-
-	//string output = "oh"; //결과를 raw 이미지로 출력합니다.
-	//output.append(".raw");
-	//FILE* outfile = fopen(output.c_str(), "w+b");
-	//for (int i = 0; i < HEIGHT; i++)
-	//{
-	//	fwrite(temp[i], sizeof(char), WIDTH, outfile);
-	//}
-	//fclose(outfile);
 	waitKey();
 }
